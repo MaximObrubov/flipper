a247edb751f7f17811c07854ad31877f7ec391df
Maxim Obrubov <m.obrubov@yandex.ru>
2023-10-04 22:30:55 +0200

    CI: test workflow (#1)
    * CI: added test workflow
    
    * Try to deploy to github pages
    
    * ci: use .min.js in index.html demo

---
+++ rollup.demo.config.js
@@ -0,0 +1,12 @@
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
+import { terser } from 'rollup-plugin-terser';
+
+
+export default {
+  input: 'index.html',
+  output: { dir: 'demo' },
+  plugins: [
+    terser(),
+    html()
+  ],
+};
